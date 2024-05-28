730103b9003b2c12bb1bdfa3502f7ae5604759be
Ritek <ritek.rounak@harness.io>
2024-05-28 23:26:54 +0530

    :hammer: Data Types Formatted

--- types/types.go
+++ types/types.go
@@ -61,19 +61,6 @@ type ProjectDetails struct {
 	Description   string   `json:"description"`
 }
 
-type BulkProjectResult struct {
-	AppName           string       `json:"appName"`
-	AppId             string       `json:"appId"`
-	ProjectIdentifier string       `json:"projectIdentifier"`
-	ProjectName       string       `json:"projectName"`
-	Error             UpgradeError `json:"error"`
-}
-
-type BulkCreateBody struct {
-	Org                  string `json:"orgIdentifier"`
-	IdentifierCaseFormat string `json:"identifierCaseFormat"`
-}
-
 type ProjectBody struct {
 	Project ProjectDetails `json:"project"`
 }
@@ -124,14 +111,6 @@ type MigrationStats struct {
 	AlreadyMigrated      int64 `json:"alreadyMigrated"`
 }
 
-type Resource struct {
-	RequestId       string                    `json:"requestId"`
-	Stats           map[string]MigrationStats `json:"stats"`
-	Errors          []UpgradeError            `json:"errors"`
-	Status          string                    `json:"status"`
-	ResponsePayload interface{}               `json:"responsePayload"`
-}
-
 type ResponseBody struct {
 	Code     interface{}        `json:"code"`
 	Message  string             `json:"message"`
@@ -149,56 +128,9 @@ type ResponseMessages struct {
 	FailureTypes interface{} `json:"failureTypes"`
 }
 
-type SaveSummary struct {
-	Stats                  map[string]MigrationStats `json:"stats"`
-	Errors                 []UpgradeError            `json:"errors"`
-	SkipDetails            []SkipDetail              `json:"skipDetails"`
-	SkippedExpressionsList []SkippedExpressionDetail `json:"skippedExpressions"`
-}
-
-type SkippedExpressionDetail struct {
-	EntityType        string   `json:"entityType"`
-	Identifier        string   `json:"identifier"`
-	OrgIdentifier     string   `json:"orgIdentifier"`
-	ProjectIdentifier string   `json:"projectIdentifier"`
-	Expressions       []string `json:"expressions"`
-}
-
-type SummaryResponse struct {
-	Summary map[string]EntitySummary `json:"summary"`
-}
-
 type SummaryDetails struct {
 	Count  int64  `json:"count"`
 	Status string `json:"status"`
-}
-
-type EntitySummary struct {
-	Name                     string                    `json:"name"`
-	Count                    int64                     `json:"count"`
-	TypeSummary              map[string]int64          `json:"typeSummary"`
-	TypesSummary             map[string]SummaryDetails `json:"typesSummary"`
-	StepTypeSummary          map[string]int64          `json:"stepTypeSummary"`
-	StepsSummary             map[string]SummaryDetails `json:"stepsSummary"`
-	KindSummary              map[string]int64          `json:"kindSummary"`
-	StoreSummary             map[string]int64          `json:"storeSummary"`
-	DeploymentTypeSummary    map[string]int64          `json:"deploymentTypeSummary"`
-	DeploymentsSummary       map[string]SummaryDetails `json:"deploymentsSummary"`
-	ArtifactsSummary         map[string]SummaryDetails `json:"artifactsSummary"`
-	CloudProviderTypeSummary map[string]int64          `json:"cloudProviderTypeSummary"`
-	Expressions              []string                  `json:"expressions"`
-}
-
-type BaseEntityDetail struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-}
-
-type ProjectCSV struct {
-	AppName           string `json:"appName"`
-	ProjectName       string `json:"projectName"`
-	ProjectIdentifier string `json:"projectIdentifier"`
-	OrgIdentifier     string `json:"orgIdentifier"`
 }
 
 type SecretStore struct {
@@ -239,96 +171,6 @@ type HarnessService struct {
 	Yaml string `json:"yaml"`
 }
 
-// GitOpsRepository structs
-type GitOpsRepository struct {
-	Repo `json:"repo"`
-}
-
-type GitOpsCluster struct {
-	Identifier        string `json:"identifier"`
-	ProjectIdentifier string `json:"projectIdentifier"`
-	OrgIdentifier     string `json:"orgIdentifier"`
-	Description       string `json:"description"`
-	Type              string `json:"type,omitempty"`
-	Cluster           `json:"cluster"`
-}
-
-type GitOpsClusterWithUpdatedFields struct {
-	Cluster       `json:"cluster"`
-	UpdatedFields []string `json:"updatedFields"`
-}
-
-type Cluster struct {
-	Name   string `json:"name"`
-	Server string `json:"server"`
-	Config `json:"config"`
-}
-
-type ClusterEnvLink struct {
-	Identifier        string `json:"identifier"`
-	OrgIdentifier     string `json:"orgIdentifier"`
-	AgentIdentifier   string `json:"agentIdentifier"`
-	ProjectIdentifier string `json:"projectIdentifier"`
-	EnvRef            string `json:"envRef"`
-	Scope             string `json:"scope"`
-}
-
-type Config struct {
-	ClusterConnectionType string `json:"clusterConnectionType"`
-}
-
-type Repo struct {
-	Repo           string `json:"repo"`
-	Type           string `json:"type"`
-	Name           string `json:"name"`
-	Project        string `json:"project,omitempty"`
-	ConnectionType string `json:"connectionType"`
-	GithubType     string `json:"githubType,omitempty"`
-	InheritedCreds bool   `json:"inheritedCreds,omitempty"`
-}
-type RepoWithUpdateMask struct {
-	Repo       `json:"repo"`
-	UpdateMask struct {
-		Paths []string `json:"paths"`
-	} `json:"updateMask"`
-}
-type UpdateMask struct {
-	Paths []string `json:"Paths"`
-}
-
-// GitOpsApplication structs
-type GitOpsApplication struct {
-	Application `json:"application"`
-}
-type Application struct {
-	Metadata `json:"metadata"`
-	Spec     `json:"spec"`
-}
-type Spec struct {
-	Source      `json:"source"`
-	Destination `json:"destination"`
-}
-type Metadata struct {
-	Name        string `json:"name"`
-	Namespace   string `json:"namespace"`
-	ClusterName string `json:"clusterName"`
-	Labels      `json:"labels"`
-	Annotations interface{}
-}
-type Labels struct {
-	Envref     string `json:"harness.io/envRef"`
-	Serviceref string `json:"harness.io/serviceRef"`
-}
-type Source struct {
-	RepoURL        string `json:"repoURL"`
-	Path           string `json:"path"`
-	TargetRevision string `json:"targetRevision"`
-}
-type Destination struct {
-	Server    string `json:"server"`
-	Namespace string `json:"namespace"`
-}
-
 type HarnessEnvironment struct {
 	Identifier        string `json:"identifier"`
 	Name              string `json:"name"`
@@ -361,7 +203,6 @@ type HarnessPipeline struct {
 	Description       string `json:"description,omitempty"`
 	Tags              struct {
 	} `json:"tags,omitempty"`
-	//Branch         string    `json:"branch,omitempty"`
 	RepoIdentifier string    `json:"repoIdentifier,omitempty"`
 	RootFolder     string    `json:"rootFolder,omitempty"`
 	FilePath       string    `json:"filePath,omitempty"`
@@ -378,63 +219,6 @@ const (
 	SSHKey            = "SSHKey"
 	WinRM             = "WinRmCredentials"
 )
-
-const (
-	SShSecretType string = "SSH"
-)
-
-const (
-	NTLM string = "NTLM"
-)
-
-type SSHSecretType struct {
-	Auth SecretAuth `json:"auth"`
-	Port int        `json:"port"`
-}
-
-type WinRMSecretType struct {
-	Auth       WinRMSecretAuth `json:"auth"`
-	Port       int             `json:"port"`
-	Parameters []string        `json:"parameters,omitempty"`
-}
-
-type SecretAuth struct {
-	Type string        `json:"type"`
-	Spec SSHSecretSpec `json:"spec"`
-}
-
-type WinRMSecretAuth struct {
-	Type string          `json:"type"`
-	Spec WinRMSecretSpec `json:"spec"`
-}
-
-type WinRMSecretSpec struct {
-	Username       string `json:"username"`
-	Password       string `json:"password"`
-	Domain         string `json:"domain"`
-	UseNoProfile   bool   `json:"useNoProfile"`
-	UseSSL         bool   `json:"useSSL"`
-	SkipCertChecks bool   `json:"skipCertChecks"`
-}
-
-type SSHSecretSpec struct {
-	CredentialType string           `json:"credentialType"`
-	Spec           SShSecretSubSpec `json:"spec"`
-}
-
-type SShSecretSubSpec struct {
-	UserName string `json:"userName"`
-	Key      string `json:"key"`
-}
-
-type SSHWINRMSecretData struct {
-	Username string
-	Key      string
-	Port     int
-	Password string
-	Domain   string
-	AuthType string
-}
 
 type CliRequest struct {
 	AuthToken   string `survey:"authToken"`
@@ -484,4 +268,25 @@ type UserInfo struct {
 		UtmTerm     string `json:"utmTerm"`
 	} `json:"utmInfo"`
 	Uuid string `json:"uuid"`
+}
+
+type Connector struct {
+	AccountIdentifier string `json:"accountIdentifier"`
+	Identifier        string `json:"identifier"`
+	Name              string `json:"name"`
+	ProjectIdentifier string `json:"projectIdentifier,omitempty"`
+	OrgIdentifier     string `json:"orgIdentifier,omitempty"`
+	Description       string `json:"description,omitempty"`
+	ConnType          string `json:"type"`
+}
+type ConnectorListBody struct {
+	Connectors []ConnectorResponse `json:"content"`
+}
+
+type ConnectorResponse struct {
+	Conn ConnectorBody `json:"connectorResponse"`
+}
+
+type ConnectorBody struct {
+	Conn Connector `json:"connector"`
 }
